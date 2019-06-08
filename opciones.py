80c0ba950c836c02718ba075455e194bcecd8433
Mariano Otero <mariano.otero@moni.com.ar>
2019-06-08 13:58:48 -0300

    agrega titulo de opciones

--- opciones.py
+++ opciones.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+
 from utilidades import *
 
 config = Configuracion()
@@ -6,11 +7,25 @@ config = Configuracion()
 layout = [
     [sg.Text('Configuraciones', size=(30, 1), font=("Helvetica", 25), text_color='blue')],
     [sg.Text('_' * 100, size=(70, 1))],
-    [sg.Text('Seleccionar el tamanio de las letras')],
-    [sg.Radio('Mayusculas', "caps", default=True), sg.Radio('Minusculas', "caps")],
-    [sg.Text('_' * 100, size=(70, 1))],
     [sg.Text('Ingrese una a una las palabras a encontrar')],
     [sg.InputText(), sg.ReadButton('Agregar')],
+    [sg.Text('_' * 100, size=(70, 1))],
+    [sg.Text('Colores (sustantivos, adjetivos, verbos)')],
+    [sg.Text('_' * 100, size=(70, 1))],
+    [sg.Text('Ayuda')],
+    [sg.Text('_' * 100, size=(70, 1))],
+    [sg.Text('Orientacion Vertical/Horizontal')],
+    [sg.Text('_' * 100, size=(70, 1))],
+    [sg.Text('Cantidad de palabras a mostrar')],
+    [sg.Text('_' * 100, size=(70, 1))],
+    [sg.Text('Mayuscula/Minuscula')],
+    [sg.Radio('Mayusculas', "caps", default=True), sg.Radio('Minusculas', "caps")],
+    [sg.Text('_' * 100, size=(70, 1))],
+    [sg.Text('Tipografia')],
+    [sg.Text('_' * 100, size=(70, 1))],
+    [sg.Text('Estilo')],
+    [sg.Text('_' * 100, size=(70, 1))],
+    [sg.Text('Oficina')],
     [sg.Text('_' * 100, size=(70, 1))],
 ]
 
