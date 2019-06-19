9ca033d2d76e8a2f07db2f33a159abc728b0147c
Almada Ortiz <juan.almada.ortiz@accenture.com>
2019-06-18 21:28:19 -0300

    Modulo Colores
    Modulo para elegir los diferentes tipos de colores a un verbo o sustantivo o adjetivo.

--- elegirColores.py
+++ elegirColores.py
@@ -1,20 +1,21 @@
 import PySimpleGUI as sg 
 
 def paleta_colores():
-    COLORS = ['blue', 'green', 'red', 'yellow', 'lightblue', 'grey', 'pink','orange']
+    
+    colors = ['BLUE', 'GREEN2', 'RED1', 'YELLOW', 'CYAN', 'GREY', 'MAGENTA','DARKORANGE1','BLACK']
 
     sg.SetOptions(button_element_size=(12,1), element_padding=(0,0), auto_size_buttons=False, border_width=0)
 
-    layout = [[sg.Text('Click on a color square to see both white and black text on that color', text_color='blue', font='Any 15')]]
+    layout = [[sg.Text('ELIJA COLOR DESEADO:', text_color='black', font='Calibri 11')]]
     row = []
     layout = []
     # -- Create primary color viewer window --
-    for rows in range(2):
+    for rows in range(3):
         row = []
-        for i in range(4):
+        for i in range(3):
             try:
-                color = COLORS[rows+2*i]
-                row.append(sg.Button(color, button_color=('black', color), key=color))
+                color = colors[rows+3*i]
+                row.append(sg.Button(border_width=0, button_color=('black', color), key=color))
             except:
                 pass
         layout.append(row)
@@ -23,26 +24,38 @@ def paleta_colores():
 
     # -- Event loop --
     while True:
-        event, values = window.Read()
+        event,values = window.Read()
         if event is None:
             break
-        # -- Create a secondary window that shows white and black text on chosen color
-        layout2 =[[sg.DummyButton(event, button_color=('white', event)), sg.DummyButton(event, button_color=('black', event))]]
-        sg.Window('Buttons with white and black text', keep_on_top=True).Layout(layout2).Read(timeout=0)
+        else:
+            if(event in colors):
+                window.Close()
+                return(event)
+     
+#layout para elegir colores a los diferentes tipos de palabras!
 
+dict_color = {}        
 
-#layout para elegir colores a los diferentes tipos de palabras!        
-
-layout =[[sg.Button('Verbo'), sg.Button('Sustantivo'), sg.Button('Adjetivo')]]
+layout =[[sg.Button('Verbo',key='verb'), sg.Button('Sustantivo',key='noun'), sg.Button('Adjetivo',key='adjetive')]]
 
 window = sg.Window('Color', grab_anywhere=False, font=('any 9')).Layout(layout)
 
 # -- Event loop --
 while True:
     event, values = window.Read()
+    print(event)
     if event is None:
         break
-    if((event == 'Verbo') or (event == 'Sustantivo') or (event=="Adjetivo")):
-        paleta_colores()
-           
-    
+    elif event == 'verb':
+        color_select = paleta_colores()
+        dict_color['Verbo']=color_select
+        print(dict_color)
+    elif event == 'noun':
+        color_select = paleta_colores()
+        dict_color['Sustantivo']=color_select
+        print(dict_color)
+    elif event == 'adjetive':
+        color_select = paleta_colores()
+        dict_color['Adjetivo']=color_select
+        print(dict_color)
+    window.Element(event).Update(button_color=(('black',('blue',color_select)[True])))
